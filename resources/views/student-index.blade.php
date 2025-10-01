2a0f38cb5e4c603a50647705f481db5e7a81f38b
Philippe Cossette <philcossette@hotmail.com>
2025-10-01 16:01:03 -0400

    Changed Card design with sliding animation for buttons

--- resources/views/student-index.blade.php
+++ resources/views/student-index.blade.php
@@ -10,14 +10,14 @@
             <div class="col-12 col-sm-6 col-md-4 col-lg-3">
                 <div class="card h-100">
                     <div class="card-header">
-                        <h5>{{ $student->last_name }} {{ $student->first_name }}</h5>
+                        <h5 class="card-title"><span>{{ $student->last_name }}</span> {{ $student->first_name }}</h5>
                         <small>{{ $student->city->name }}</small>
                     </div>
                     <div class="card-body text-center">
                         <img src="https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png"
                             alt="Portrait" class="img-fluid rounded-circle" style="max-width: 100px;">
                     </div>
-                    <div class="card-footer d-flex justify-content-between">
+                    <div class="sliding-card-footer card-footer d-flex justify-content-between">
                         <button class="btn btn-outline-primary btn-sm">Voir les détails</button>
                         <button class="btn btn-danger btn-sm">Supprimer</button>
                     </div>
